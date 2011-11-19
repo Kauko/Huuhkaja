afbb1b482afde4f4d75a205aa149c84557d3aae6
Jukka Pietila <jukkapietila@gmail.com>
2011-11-19 04:10:44 +0200

    Made the bare-boned version for Bluetooth handling. More to follow.

--- bluetooth.cpp
+++ bluetooth.cpp
@@ -1,9 +1,39 @@
+
 #include "bluetooth.h"
 
 Bluetooth::Bluetooth(QObject *parent) : QObject(parent) {
+    //initialize objects and values
     qDebug("Bluetooth Construct");
+    btoothLocDevice = new QtMobility::QBluetoothLocalDevice(this);
+    btoothAddress = new QtMobility::QBluetoothAddress::QBluetoothAddress(btoothLocDevice->address());
+    discoveryAgent = new QtMobility::QBluetoothDeviceDiscoveryAgent(this);
+    discoveredDevices.clear();  //to ensure that the list is clear, damnit
+
 }
 
 Bluetooth::~Bluetooth() {
     qDebug("Bluetooth Destruct");
+}
+
+void Bluetooth::update()
+{
+    //first, check if enough time has passed since last polling available btooth devices
+        //if enough, poll again
+        //add new devices to list and remove devices which were not found
+    //then, if we polled again, we check the signal strength for all devices we discovered
+
+}
+
+///
+///Start the device polling
+///
+void Bluetooth::start()
+{
+    discoveryAgent->start();
+}
+
+///
+///Stop device polling
+void Bluetooth::stop(){
+    discoveryAgent->stop();
 }
--- bluetooth.h
+++ bluetooth.h
@@ -2,14 +2,27 @@
 #define BLUETOOTH_H
 
 #include <QObject>
+#include <QList>
+#include <qbluetooth.h>
+#include <QBluetoothLocalDevice>
+#include <QBluetoothAddress>
+#include <QBluetoothDeviceInfo>
+#include <QBluetoothDeviceDiscoveryAgent>
 #include "singleton.h"
 
 class Bluetooth : public QObject, public Singleton<Bluetooth> {
     Q_OBJECT
 public:
     explicit Bluetooth(QObject *parent = 0);
+    void update();
+    void start();
+    void stop();
     ~Bluetooth();
 private:
+    QtMobility::QBluetoothAddress *btoothAddress; //device's bluetooth address
+    QtMobility::QBluetoothLocalDevice *btoothLocDevice; //local device, used for retrieving for example MAC-address and such
+    QtMobility::QBluetoothDeviceDiscoveryAgent *discoveryAgent; //used for discovering other devices
+    QList<QtMobility::QBluetoothDeviceInfo> discoveredDevices; //devices discovered, duh
 
 signals:
 
