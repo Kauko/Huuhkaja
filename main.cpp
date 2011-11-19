98f2f8de86c77048a6cb0a1c026eaff9ce13e3d4
Antti Tönkyrä <daedalus@pingtimeout.net>
2011-11-19 02:25:58 +0200

    Main

--- main.cpp
+++ main.cpp
@@ -2,13 +2,24 @@
 
 #include <QtGui/QApplication>
 
+#include "accelerometer.h"
+#include "bluetooth.h"
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    Accelerometer::init(0);
+    Bluetooth::init(0);
 
     MainWindow mainWindow;
     mainWindow.setOrientation(MainWindow::ScreenOrientationLockPortrait);
     mainWindow.showExpanded();
 
-    return app.exec();
+    int returnCode = app.exec();
+
+    Accelerometer::destroy();
+    Bluetooth::destroy();
+
+    return returnCode;
 }
